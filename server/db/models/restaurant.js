d6631cbfa1b000d7d118861c275fae4bb9e78c62
James O'Reilly <James.Oreilly926@gmail.com>
2017-11-16 15:19:10 -0500

    feat: create restaurant model in db/models/restaurant.js

---
+++ server/db/models/restaurant.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Restaurant = db.define('restaurant', {
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  zipCode: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  phoneNumber: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+})
+
+module.exports = Restaurant
