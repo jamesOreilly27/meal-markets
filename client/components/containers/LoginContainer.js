1a21e0f0f8c2fca0d962f548b40b0a0ed8681d16
sethfork <sethfork@users.noreply.github.com>
2017-11-16 15:37:38 -0500

    Chore: Initial cleanup - Configure boilerplate to specs

---
+++ client/components/containers/LoginContainer.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux'
+import { auth } from '../store'
+import AuthFormPresenter from '../presenters/AuthFormPresenter'
+
+const mapState = state => ({
+  displayName: 'Login',
+  error: state.user.error
+})
+
+const mapDispatch = dispatch => ({
+  handleSubmit (name, email, password) {
+    dispatch(auth(email, password, name))
+  }
+})
+
+export const Login = connect(mapState, mapDispatch)(AuthFormPresenter)
---
+++ client/components/containers/SignupContainer.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux'
+import { auth } from '../store'
+import AuthFormPresenter from '../presenters/AuthFormPresenter'
+
+const mapState = state => ({
+  displayName: 'Sign Up',
+  error: state.user.error
+})
+
+const mapDispatch = dispatch => ({
+  handleSubmit (name, email, password, paymentInfo) {
+    dispatch(auth(email, password, name, paymentInfo))
+  }
+})
+
+const SignupContainer = connect(mapState, mapDispatch)(AuthFormPresenter)
+export default SignupContainer
