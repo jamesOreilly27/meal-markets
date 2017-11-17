09336822db8b44c337c00beba914fb25b108aabc
Bobby Lux <robert.michael.lux@gmail.com>
2017-11-17 13:03:04 -0500

    test: tag association testing

--- server/db/models/tag.spec.js
+++ server/db/models/tag.spec.js
@@ -1,6 +1,8 @@
 const { expect } = require('chai')
 const db = require('../index')
 const Tag = db.model('tag')
+const Meal = db.model('meal')
+const MealTags = db.model('MealTags')
 
 describe('Tag model', () => {
   beforeEach(() => {
@@ -8,12 +10,36 @@ describe('Tag model', () => {
   })
 
   describe('associations', () => {
+    let testMeal
 
     beforeEach(() => {
-      return Tag.create({
-        type: 'Japanese',
-        tagId: 1
+      console.log('RUNNING!')
+      return Meal.create({
+        name: 'Pokebowl',
+        inStorePrice: 1000,
+        basePrice: 900,
+      })
+      .then(meal => {
+        testMeal = meal
+        return Tag.create({
+          type: 'Hawaiian',
+        })
+        .then(tag => {
+          testMeal.addTag(tag)
+        })
       })
     })
+
+    it('testMeal has one tag association in the join table', () => {
+      MealTags.findAll()
+      .then(found => console.log('All', found))
+      MealTags.findAll({where: {
+        mealId: testMeal.id
+      }})
+      .then(foundMealTags => {
+        expect(foundMealTags.length).to.be.equal(1)
+      })
+    })
+
   })
 })
