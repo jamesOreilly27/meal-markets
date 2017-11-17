ba0591147392790e84e533855bab2e67e9104762
James O'Reilly <James.Oreilly926@gmail.com>
2017-11-16 22:09:06 -0500

    install chalk

--- script/seed.js
+++ script/seed.js
@@ -1,22 +1,10 @@
-/**
- * Welcome to the seed file! This seed file uses a newer language feature called...
- *
- *                  -=-= ASYNC...AWAIT -=-=
- *
- * Async-await is a joy to use! Read more about it in the MDN docs:
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
- *
- * Now that you've got the main idea, check it out in practice below!
- */
 const db = require('../server/db')
 const {User} = require('../server/db/models')
+const chalk = require('chalk')
 
 async function seed () {
   await db.sync({force: true})
-  console.log('db synced!')
-  // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
-  // executed until that promise resolves!
+  console.log(chalk.bgBlue.white.bold('db synced!'))
 
   const users = await Promise.all([
     User.create({email: 'cody@email.com', password: '123'}),
