4a79e3b317fe8f7ef088066e2565b6de5f6e46cc
sethfork <sethfork@users.noreply.github.com>
2017-11-17 17:14:00 -0500

    Chore: Change main to Navbar

--- client/routes.js
+++ client/routes.js
@@ -1,70 +1,53 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import history from './history'
-import {Main, Login, Signup, UserHome, EatersLanding } from './components'
+import {Navbar, Login, Signup, UserHome, EatersLanding } from './components'
 import {me} from './store'
 
-/**
- * COMPONENT
- */
 class Routes extends Component {
   componentDidMount () {
     this.props.loadInitialData()
   }
 
   render () {
-    const {isLoggedIn} = this.props
-
+    const { isLoggedIn } = this.props
     return (
-      <Router history={history}>
-        <Main>
+      <div>
+        <Navbar />
+        <div>
           <Switch>
-            {/* Routes placed here are available to all visitors */}
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
             {
               isLoggedIn &&
                 <Switch>
-                  {/* Routes placed here are only available after logging in */}
                   <Route path="/home" component={UserHome} />
                   <Route path="/eat" component={EatersLanding} />
                 </Switch>
             }
-            {/* Displays our Login component as a fallback */}
             <Route component={Login} />
           </Switch>
-        </Main>
-      </Router>
+        </div>
+      </div>
     )
   }
 }
 
-/**
- * CONTAINER
- */
-const mapState = (state) => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
-  }
-}
+const mapState = state => ({
+  // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
+  // Otherwise, state.user will be an empty object, and state.user.id will be falsey
+  isLoggedIn: !!state.user.id
+})
 
-const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData () {
-      dispatch(me())
-    }
+const mapDispatch = dispatch => ({
+  loadInitialData () {
+    dispatch(me())
   }
-}
+})
 
 export default connect(mapState, mapDispatch)(Routes)
 
-/**
- * PROP TYPES
- */
 Routes.propTypes = {
   loadInitialData: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
