0a28ad8fadc76645493972958dc58257d1736d6e
Aaron Wong <aaronw2291@gmail.com>
2017-11-20 18:28:11 -0500

    Update users.js
    Remove console logs.

--- server/api/users.js
+++ server/api/users.js
@@ -15,8 +15,6 @@ router.get('/', (req, res, next) => {
 })
 
 router.get('/:userId/redeemable', isSelf, (req, res, next) => {
-  console.log('REQ PARAMS', req.params.userId)
-  console.log('REQ USER', req.user.id)
   Order.findAll({ where: { userId: req.params.userId } })
   .then(orders => {
     res.json(orders.filter(order => order.redeemable))
