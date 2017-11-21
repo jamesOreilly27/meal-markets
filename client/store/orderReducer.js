3b4864d1fd401eab83074f35dd10d53198d469f5
James O'Reilly <James.Oreilly926@gmail.com>
2017-11-21 11:43:13 -0500

    add redux for fetching redeemable orders

--- client/store/orderReducer.js
+++ client/store/orderReducer.js
@@ -1,11 +1,18 @@
 import axios from 'axios'
 
 export const NEW_ORDER = 'CREATE_ORDER'
+export const GOT_ORDERS = 'GOT_ORDERS'
 
 export const newOrder = order => ({
   type: NEW_ORDER,
   order
 })
+
+export const gotOrders = orders => ({
+  type: GOT_ORDERS,
+  orders
+})
+
 
 export const createOrder = (user, meal, currentPrice) =>
   dispatch =>
@@ -19,8 +26,16 @@ export const createOrder = (user, meal, currentPrice) =>
       dispatch(newOrder(res.data)))
     .catch(err => console.error('Error creating order: ', err))
 
+export const getRedeemableOrders = user => dispatch => (
+  axios.get(`apii/users/${user.id}/redeemable`)
+  .then(orders => dispatch(gotOrders(orders)))
+  .catch(err => dispatch(gotOrders(err)))
+)
+
 export default (orders = [], action) => {
   switch (action.type) {
+    case GOT_ORDERS:
+      return action.orders
     case NEW_ORDER:
       return [...orders, action.order]
     default:
