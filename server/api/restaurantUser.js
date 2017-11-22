b87ac9b59262d5bf3e6995d045fe93a0e22aa4e8
James O'Reilly <James.Oreilly926@gmail.com>
2017-11-22 12:39:00 -0500

    feat: create GET route for /api/users/owner/:id/open-orders

--- server/api/restaurantUser.js
+++ server/api/restaurantUser.js
@@ -11,12 +11,21 @@ router.get('/:id', (req, res, next) => {
   .catch(next)
 })
 
-router.get('/:id/orders', (req, res, next) => {
+router.get('/:id/open-orders', (req, res, next) => {
+  RestaurantUser.scope('orders').findById(req.params.id)
+  .then(user => user.orders.filter(
+    order => !order.fulfilled
+  ))
+  .then(openOrders => res.json(openOrders))
+  .catch(next)
+})
+
+router.get('/:id/todays-orders', (req, res, next) => {
   RestaurantUser.scope('orders').findById(req.params.id)
   .then(user => user.orders.filter(
     order => order.redeemable && !order.fulfilled
   ))
-  .then(filteredOrders => res.json(filteredOrders))
+  .then(todaysOrders => res.json(todaysOrders))
   .catch(next)
 })
 
