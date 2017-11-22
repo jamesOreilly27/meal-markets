29711433ca32d1216ee3f5afd9974a1a66f1184c
sethfork <sethfork@users.noreply.github.com>
2017-11-22 12:56:46 -0500

    Feat: VictoryContainer, currentMealReducer
    - Make VictoryContainer smarter / more flexible
    - getCurrentMeal -> setCurrentMeal
    - Use defaultMeal object with defaultMeal bool in reducer only, eliminating hardcoding

--- client/store/currentMealReducer.js
+++ client/store/currentMealReducer.js
@@ -1,13 +1,19 @@
-export const GET_CURRENT_MEAL = 'GET_CURRENT_MEAL'
+export const SET_CURRENT_MEAL = 'SET_CURRENT_MEAL'
 
-export const getCurrentMeal = meal => ({
-  type: GET_CURRENT_MEAL,
+export const setCurrentMeal = meal => ({
+  type: SET_CURRENT_MEAL,
   meal
 })
 
-export default (currentMeal = {}, action) => {
+const defaultMeal = {
+  defaultMeal: true,
+  basePrice: 8,
+  inStorePrice: 12
+}
+
+export default (currentMeal = defaultMeal, action) => {
   switch (action.type) {
-    case GET_CURRENT_MEAL:
+    case SET_CURRENT_MEAL:
       return action.meal
     default:
       return currentMeal
