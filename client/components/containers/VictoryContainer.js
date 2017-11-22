9fa64171b0e9ff11a6535a99cd50167c990e0cb9
Aaron Wong <aaronw2291@gmail.com>
2017-11-22 11:54:30 -0500

    Update VictoryContainer.js

--- client/components/containers/VictoryContainer.js
+++ client/components/containers/VictoryContainer.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import VictoryPresenter from '../presenters/VictoryPresenter'
 const fakeMeal = { basePrice: 8, inStorePrice: 12 }
-
+// NEED TO GET RID OF FAKE MEAL AND CHANGE TEAL TO MEAL
 const Victory = ({ meal = fakeMeal }) => {
   const data = new Array(30).fill(0).map((el, index) => ({
     basePrice: meal.basePrice,
@@ -14,6 +14,7 @@ const Victory = ({ meal = fakeMeal }) => {
 }
 
 const mapState = state => ({
+  // NEED TO CHANGE TEAL TO MEAL
   teal: state.currentMeal
 })
 
