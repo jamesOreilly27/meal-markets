2d8ebb21e8d47e1fedc77adae3d980dea4281fdf
Aaron W <aaronw2291@gmail.com>
2017-11-28 13:52:40 -0500

    feat(buy-panel): set up front-end elements

--- client/components/presenters/BuyPanelPresenter.js
+++ client/components/presenters/BuyPanelPresenter.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
 const BuyPanel = () => (
   <div>
     <h2>Buy Panel</h2>
     <p>Use this panel to make bulk purchases of the currently selected meal</p>
+
+    <select>
+      <option value={1}>Meal - Price: $10.00</option>
+    </select>
+
+    <Button onClick={() => console.log('Hello!')}>Buy</Button>
   </div>
 )
 
-export default BuyPanel
+const mapState = state => ({
+
+})
+
+const mapDispatch = dispatch => {
+
+}
+
+const BuyPanelContainer = withRouter(connect(mapState, mapDispatch)(BuyPanel))
+export default BuyPanelContainer
