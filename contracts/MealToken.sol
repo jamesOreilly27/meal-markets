f97e5036bd4b002ae5304142c107a14b7685249d
sethfork <sethfork@users.noreply.github.com>
2017-11-28 14:03:06 -0500

    Use zeppelin contracts

--- contracts/MealToken.sol
+++ contracts/MealToken.sol
@@ -1,34 +1,51 @@
-pragma solidity ^0.4.18;
+// pragma solidity ^0.4.18;
 
-import "./ConvertLib.sol";
+// import "./ConvertLib.sol";
 
-// This is just a simple example of a coin-like contract.
-// It is not standards compatible and cannot be expected to talk to other
-// coin/token contracts. If you want to create a standards-compliant
-// token, see: https://github.com/ConsenSys/Tokens. Cheers!
+// // This is just a simple example of a coin-like contract.
+// // It is not standards compatible and cannot be expected to talk to other
+// // coin/token contracts. If you want to create a standards-compliant
+// // token, see: https://github.com/ConsenSys/Tokens. Cheers!
 
-contract MealToken {
-    mapping (address => uint) balances;
+// contract MealToken {
+//     mapping (address => uint) balances;
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+//     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-    function MealToken() public {
-        balances[tx.origin] = 10000;
-    }
+//     function MealToken() public {
+//         balances[tx.origin] = 10000;
+//     }
 
-    function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
-        if (balances[msg.sender] < amount) return false;
-        balances[msg.sender] -= amount;
-        balances[receiver] += amount;
-        Transfer(msg.sender, receiver, amount);
-        return true;
-    }
+//     function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
+//         if (balances[msg.sender] < amount) return false;
+//         balances[msg.sender] -= amount;
+//         balances[receiver] += amount;
+//         Transfer(msg.sender, receiver, amount);
+//         return true;
+//     }
 
-    function getBalanceInEth(address addr) public view returns(uint) {
-        return ConvertLib.convert(getBalance(addr), 2);
-    }
+//     function getBalanceInEth(address addr) public view returns(uint) {
+//         return ConvertLib.convert(getBalance(addr), 2);
+//     }
 
-    function getBalance(address addr) public view returns(uint) {
-        return balances[addr];
+//     function getBalance(address addr) public view returns(uint) {
+//         return balances[addr];
+//     }
+// }
+
+pragma solidity ^0.4.4;
+
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+
+
+contract MealToken is StandardToken {
+    string public name = 'MealToken';
+    string public symbol = 'MEAL';
+    uint public decimals = 2;
+    uint public INITIAL_SUPPLY = 12000;
+
+    function MealToken() {
+        totalSupply = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,13 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MealToken = artifacts.require("./MealToken.sol");
+// var MealToken = artifacts.require("./MealToken.sol");
+var MealToken = artifacts.require("MealToken");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MealToken);
   deployer.deploy(MealToken);
 };
+
+// module.exports = function(deployer) {
+//   deployer.deploy(ConvertLib);
+//   deployer.link(ConvertLib, MealToken);
+//   deployer.deploy(MealToken);
+// };
